786ab16ae5a73286af066713d32788ba6834f0fe
hhg21011998 <hhg21011998@gmail.com>
2022-07-28 17:11:39 +0700

    commited

--- ps1/ps1.py
+++ ps1/ps1.py
@@ -32,7 +32,7 @@
 # portion_down_payment = 0.25
 # total_cost = total_cost * portion_down_payment
 # current_savings = 0
-# r = 0.04
+# return_annual = 0.04
 # month_salary = annual_salary / 12
 # month_savings = month_salary * portion_saved
 
@@ -40,7 +40,7 @@
 
 # while current_savings < total_cost:
 #     number_of_months += 1
-#     current_savings += current_savings * r / 12
+#     current_savings += current_savings * return_annual / 12
 #     current_savings += month_savings
 #     if number_of_months > 0 and number_of_months % 6 == 0:
 #         annual_salary = annual_salary + (annual_salary * semi_annual_raise)
@@ -53,39 +53,51 @@
 
 # Part C: Finding the right amount to save away
 
-# startingSalary = int(input("Enter the starting salary: "))
-# semiAnnualRaise = 0.07
-# r = 0.04
-# portionDownPayment = 0.25
-# costOfHouse = 1000000
-# totalCostNeeding = costOfHouse * portionDownPayment
-# monthSalary = startingSalary / 12
+semi_annual_raise = 0.07 
+return_annual = 0.04
+down_payment_house = 750000
+annual_salary = 150000
 
-# epsilon = 100
+lower = 0
+higher = 10000
+epsilon = 100
 
-# low = 0
-# high = 10000
-# guessRate = float((low + high) / 2)
+step_biSearch = 0
+possible = True
 
-# numberOfGuess = 0
-# number_of_months = 0
-# currentSaving = 0
-
-
-# while number_of_months <= 36:
-#     number_of_months += 1
-#     currentSaving += currentSaving * r / 12
-#     currentSaving += monthSalary * (guessRate / 10000)
+while True:
+    step_biSearch =+ 1
+    portion_saved = (lower + higher) / 2 / 10000
+    month_salary = annual_salary / 12
+    month_savings = month_salary * portion_saved
     
-epsilon = 0.001
-low = 1
-high = 2
-x = high
-while abs((x**2 - 3) - 0) >= epsilon :
-    if x**2 - 3 - 0 > epsilon:
-        high = x
+    current_savings = 0
+    three_years = 0
+    
+    while three_years <= 36:
+        three_years += 1
+        current_savings += current_savings * return_annual / 12
+        current_savings += month_savings
+        
+        if three_years % 6 == 0:
+            annual_salary = annual_salary + (annual_salary * semi_annual_raise)
+            month_salary = annual_salary / 12
+            month_savings = month_salary * portion_saved    
+    if abs(current_savings - down_payment_house) <= 100:
+        break
+    
+    if current_savings > down_payment_house:
+        higher = portion_saved * 10000
     else:
-        low = x
-    x = (low + high) / 2
-print(x)
+        lower = portion_saved * 10000
+    
+    if lower >= higher:
+        possible = False
+        break
+    
 
+if possible:
+    print("Step of Bisection:", step_biSearch)
+    print("Portion Saving:", portion_saved)
+else:
+    print("It's impossible")
